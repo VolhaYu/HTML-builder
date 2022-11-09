fad6d891f9eee03b9e4e98656305a2adff631112
VolhaYu <olenka_glushko1993@mail.ru>
2022-11-09 14:32:22 +0300

    fix: remove error

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,29 +1,58 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const fsPromise = require("fs/promises");
 
-function copyDir() {
-    fs.rm('04-copy-directory/files-copy', {recursive: true}, err => {
-        if(err) console.log(err);
-     });
+async function copyDir() {
+  try {
+    await fs.promises.access(path.join(__dirname, "files-copy"));
+    await fsPromise.rm(
+      "04-copy-directory/files-copy",
+      { recursive: true },
+      (err) => {
+        if (err) console.log(err);
+      }
+    );
+    await fsPromise.mkdir(
+      path.join(__dirname, "files-copy"),
+      { recursive: true },
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  } catch (error) {
+    await fsPromise.mkdir(
+      path.join(__dirname, "files-copy"),
+      { recursive: true },
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
 
-    fs.mkdir(path.join(__dirname,'files-copy'), {recursive: true}, err => {
-        if(err) throw err;
-    });
-
-    fs.readdir('04-copy-directory/files', {withFileTypes: true}, (err, files) => {
-        if(!err) {
-            files.forEach((file) => {
-                fs.readFile(path.join(__dirname, 'files',file.name), 'utf-8', (err, fileContent) => {
-                    if (err) console.log(err);
-                    const filesFolder = path.join(__dirname, 'files', file.name);
-                    fs.copyFile(filesFolder,
-                                path.join(__dirname, 'files-copy', file.name), (err) => {
-                                    if(err) console.log(err);
-                                });
-                    
-                });
-            });
-        }
-    });   
+  fs.readdir(
+    "04-copy-directory/files",
+    { withFileTypes: true },
+    (err, files) => {
+      if (!err) {
+        files.forEach((file) => {
+          fs.readFile(
+            path.join(__dirname, "files", file.name),
+            "utf-8",
+            (err, fileContent) => {
+              if (err) console.log(err);
+              const filesFolder = path.join(__dirname, "files", file.name);
+              fs.copyFile(
+                filesFolder,
+                path.join(__dirname, "files-copy", file.name),
+                (err) => {
+                  if (err) console.log(err);
+                }
+              );
+            }
+          );
+        });
+      }
+    }
+  );
 }
 copyDir();
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,76 @@
-const fs = require('fs');
-const path = require('path');
-const { stdin } = require('process');
-fs.unlink('project-dist/bundle.css', err => {
-    if(err) console.log(err);
- });
+const fs = require("fs");
+const path = require("path");
+const { stdin } = require("process");
+const fsPromise = require("fs/promises");
 
-const output = fs.createWriteStream(path.join(__dirname, 'project-dist/bundle.css'));
-const newFile =[];
-fs.readdir('05-merge-styles/styles', {withFileTypes: true}, (err, files) => {
-    if(!err) {
+async function mergeStyles() {
+  try {
+    await fs.promises.access(path.join(__dirname, "project-dist/bundle.css"));
+    await fs.promises.unlink("project-dist/bundle.css", (err) => {
+      if (err) console.log(err);
+    });
+    fs.createWriteStream(path.join(__dirname, "project-dist/bundle.css"));
+  } catch (error) {
+    fs.createWriteStream(path.join(__dirname, "project-dist/bundle.css"));
+  }
+  const output = fs.createWriteStream(
+    path.join(__dirname, "project-dist/bundle.css")
+  );
+  const newFile = [];
+  fs.readdir(
+    "05-merge-styles/styles",
+    { withFileTypes: true },
+    (err, files) => {
+      if (!err) {
         files.forEach((file) => {
-            fs.readFile(path.join(__dirname, 'styles',file.name), 'utf-8', (err, fileContent) => {
-                if (err) console.log(err);
-                const ext = path.extname(path.join(__dirname,'styles', file.name));
-                if(file.isFile()) {
-                    if(ext === '.css') {
-                        newFile.push(fileContent);
-                        output.write(newFile.join('').toString());
-                    }
-                }                
-            });
+          fs.readFile(
+            path.join(__dirname, "styles", file.name),
+            "utf-8",
+            (err, fileContent) => {
+              if (err) console.log(err);
+              const ext = path.extname(
+                path.join(__dirname, "styles", file.name)
+              );
+              if (file.isFile()) {
+                if (ext === ".css") {
+                  newFile.push(fileContent);
+                  output.write(newFile.join("").toString());
+                  //   fs.createWriteStream(
+                  //     path.join(__dirname, "project-dist/bundle.css")
+                  //   ).write(newFile.join("").toString());
+                }
+              }
+            }
+          );
         });
+      }
     }
-});
+  );
+}
+mergeStyles();
+// fs.unlink('project-dist/bundle.css', err => {
+//     if(err) console.log(err);
+//  });
+
+// const output = fs.createWriteStream(path.join(__dirname, 'project-dist/bundle.css'));
+// const newFile = [];
+// fs.readdir("05-merge-styles/styles", { withFileTypes: true }, (err, files) => {
+//   if (!err) {
+//     files.forEach((file) => {
+//       fs.readFile(
+//         path.join(__dirname, "styles", file.name),
+//         "utf-8",
+//         (err, fileContent) => {
+//           if (err) console.log(err);
+//           const ext = path.extname(path.join(__dirname, "styles", file.name));
+//           if (file.isFile()) {
+//             if (ext === ".css") {
+//               newFile.push(fileContent);
+//               output.write(newFile.join("").toString());
+//             }
+//           }
+//         }
+//       );
+//     });
+//   }
+// });
